ebd4016e82fcf3dca97ef6d3788486503b57e418
NG <guraband@naver.com>
2023-04-08 00:11:38 +0900

    #6.13 Futures 완료

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
   const DetailScreen({
     super.key,
@@ -9,9 +12,23 @@ class DetailScreen extends StatelessWidget {
     required this.id,
   });
 
-  // final Future<WebtoonDetailModel> webtoons = ApiService.getToonById(id);
-  // final Future<List<WebtoonEpisodeModel>> episodes =
-  // const ApiService.getLatestEpisodesById(id, {super.key});
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late final Future<WebtoonDetailModel> webtoon;
+  late final Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+
+    print(webtoon);
+    print(episodes);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +39,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w500,
@@ -38,7 +55,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 350,
                   clipBehavior: Clip.hardEdge,
@@ -53,7 +70,7 @@ class DetailScreen extends StatelessWidget {
                     ],
                   ),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                     headers: const {
                       "User-Agent":
                           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
